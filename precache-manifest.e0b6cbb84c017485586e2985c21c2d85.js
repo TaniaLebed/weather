b12e65ee2ae1b62e8de52772ad6d8c77673623d8
TaniaLebed <lebedtania666@gmail.com>
2020-09-14 10:44:07 +0300

    Updates

---
+++ precache-manifest.e0b6cbb84c017485586e2985c21c2d85.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6167c3e95e1a38268e0b490e9ade414",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "6c466c4fe8328ba7bcdf",
+    "url": "/weather/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "8ff125bb27645c51798f",
+    "url": "/weather/static/css/main.914973b3.chunk.css"
+  },
+  {
+    "revision": "6c466c4fe8328ba7bcdf",
+    "url": "/weather/static/js/2.bfcc7af3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/weather/static/js/2.bfcc7af3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ff125bb27645c51798f",
+    "url": "/weather/static/js/main.5faff127.chunk.js"
+  },
+  {
+    "revision": "c99e76bcf2bdae1a8b8a",
+    "url": "/weather/static/js/runtime-main.953fba7f.js"
+  },
+  {
+    "revision": "cc54c19b29890841267ee0750eb25a00",
+    "url": "/weather/static/media/polygon.cc54c19b.jpg"
+  }
+]);
